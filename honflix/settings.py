382197e66604a11003296366f1a47cc7b339ac82
August.J <96563144+Augustj88@users.noreply.github.com>
2022-03-27 00:01:26 +0900

    Update settings.py

--- honflix/settings.py
+++ honflix/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uvo2da#j!bk1epbi$-m$d(ueozde1o3-#%!v!-a%93ulhe1ti7'
-
+SECRET_KEY = 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
